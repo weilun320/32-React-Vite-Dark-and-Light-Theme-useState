db984bbc2248bee5eca639dc64d9b6f77635e1fd
Wei Lun <41337787+weilun320@users.noreply.github.com>
2023-12-11 21:04:09 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/32-React-Vite-Light-and-Dark-Theme-useState/"
+  base: "/32-React-Vite-Dark-and-Light-Theme-useState/"
 })
