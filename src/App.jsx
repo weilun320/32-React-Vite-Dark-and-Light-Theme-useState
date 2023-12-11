e9faee4a80899efecd68f32a182413e6b6c118b6
Wei Lun <weilun9320@hotmail.com>
2023-12-11 17:27:42 +0800

    Added Theme switcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  return (
-    <>
+  const [isDarkTheme, setDarkTheme] = useState(false);
 
-    </>
-  )
+  function switchTheme() {
+    if (isDarkTheme) {
+      setDarkTheme(false);
+    }
+    else {
+      setDarkTheme(true);
+    }
+  }
+
+  return (
+    <div className={isDarkTheme ? "dark" : "light"}>
+      <h1>Theme Switcher</h1>
+      <p>{isDarkTheme ? "Dark Theme" : "Light Theme"}</p>
+      <button onClick={switchTheme}>Toggle Theme</button>
+    </div>
+  );
 }
 
 export default App
